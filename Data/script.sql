ae40c16e394c8da9eae09d26832487222ea4e3e6
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2023-12-05 08:28:01 -0700

    Actividad 3.1

---
+++ Data/script.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" TEXT NOT NULL CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY,
+    "ProductVersion" TEXT NOT NULL
+);
+
+BEGIN TRANSACTION;
+
+CREATE TABLE "Alumnos" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Alumnos" PRIMARY KEY AUTOINCREMENT,
+    "Matricula" TEXT NOT NULL,
+    "Nombres" TEXT NOT NULL,
+    "Apellidos" TEXT NOT NULL,
+    "CarreraId" INTEGER NOT NULL
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20231026143710_InitialCreate', '7.0.12');
+
+COMMIT;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE "Carreras" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Carreras" PRIMARY KEY AUTOINCREMENT,
+    "Nombre" TEXT NOT NULL
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20231031145223_AddTableCarreras', '7.0.12');
+
+COMMIT;
+
